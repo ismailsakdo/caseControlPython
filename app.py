9405825348c0880367f62ff488b6492f1d233cba
Kajidataonline <57325497+ismailsakdo@users.noreply.github.com>
2023-07-28 23:34:51 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import pandas as pd
+from pandas_profiling import ProfileReport
+from scipy.stats import chi2_contingency
+
+# Function to generate the Pandas Profiling report
+def generate_profile_report(data):
+    profile = ProfileReport(data, title='Pandas Profiling Report', explorative=True)
+    return profile.to_html()
+
+# Function to calculate odds ratio and perform chi-square test
+def calculate_odds_ratio_and_chi2(data, food_type):
+    contingency_table = pd.crosstab(data['case_or_control'], data[food_type].map({'eat': True, 'not eat': False}))
+    if 'Case' in contingency_table.index and 'Control' in contingency_table.index:
+        odds_ratio = contingency_table.loc['Case', True] * contingency_table.loc['Control', False] / (contingency_table.loc['Control', True] * contingency_table.loc['Case', False])
+        chi2, p, dof, expected = chi2_contingency(contingency_table)
+    else:
+        odds_ratio = chi2 = p = dof = None
+    return odds_ratio, chi2, p
+
+# Function to generate a report displaying chi-square results and odds ratios for each food type
+def generate_chi_square_report(data):
+    food_types = ['foodA', 'foodB', 'foodC', 'foodD', 'foodE']
+    results = []
+
+    for food in food_types:
+        odds_ratio, chi2, p = calculate_odds_ratio_and_chi2(data, food)
+        results.append({
+            'Food Type': food,
+            'Odds Ratio': odds_ratio,
+            'Chi-square': chi2,
+            'P-value': p
+        })
+
+    report_df = pd.DataFrame(results)
+    return report_df
+
+def main():
+    st.title("Epidemiology Data Analysis App")
+
+    # Upload CSV file
+    uploaded_file = st.file_uploader("Upload CSV file", type=['csv'])
+
+    if uploaded_file is not None:
+        # Read the CSV file into a DataFrame
+        data = pd.read_csv(uploaded_file)
+
+        # Display the raw data
+        st.subheader("Raw Data")
+        st.write(data)
+
+        # Generate and display the Pandas Profiling report
+        if st.sidebar.checkbox("Show Pandas Profiling Report"):
+            st.subheader("Pandas Profiling Report")
+            profile_html = generate_profile_report(data)
+            st.components.v1.html(profile_html, height=600, scrolling=True)
+
+        # Show menu to select a specific food type for analysis
+        st.subheader("Food Type Analysis")
+        food_types = ['foodA', 'foodB', 'foodC', 'foodD', 'foodE']
+        selected_food = st.selectbox("Select a food type", food_types)
+
+        # Calculate odds ratio and perform chi-square test for the selected food type
+        odds_ratio, chi2, p = calculate_odds_ratio_and_chi2(data, selected_food)
+
+        # Display odds ratio and chi-square test results
+        if odds_ratio is not None and chi2 is not None:
+            st.subheader("Odds Ratio")
+            st.write(f"Odds Ratio for Food Type '{selected_food}': {odds_ratio}")
+
+            st.subheader("Chi-square Test Results")
+            st.write(f"Chi-square statistic: {chi2}")
+            st.write(f"P-value: {p}")
+
+        # Generate and display the simple report with chi-square results and odds ratios
+        if st.sidebar.checkbox("Show Overall Result"):
+            report_df = generate_chi_square_report(data)
+            st.subheader("Chi-square Results and Odds Ratios")
+            st.dataframe(report_df)
+
+if __name__ == "__main__":
+    main()
