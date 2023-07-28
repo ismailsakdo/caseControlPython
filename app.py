4b61aa2e94ac6a51d495711f68a2d6997ee2f947
Kajidataonline <57325497+ismailsakdo@users.noreply.github.com>
2023-07-29 00:28:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,26 @@ import pandas as pd
 from pandas_profiling import ProfileReport
 from scipy.stats import chi2_contingency
 
-# Function to generate the Pandas Profiling report
-def generate_profile_report(data):
-    profile = ProfileReport(data, title='Pandas Profiling Report', explorative=True)
+# Function to generate the Descriptive Statistics report
+def generate_descriptive_statistics(data):
+    profile = ProfileReport(data, title='Descriptive Statistics of the Data', explorative=True)
     return profile.to_html()
 
 # Function to calculate odds ratio and perform chi-square test
 def calculate_odds_ratio_and_chi2(data, food_type):
-    contingency_table = pd.crosstab(data['case_or_control'], data[food_type].map({'eat': True, 'not eat': False}))
-    if 'Case' in contingency_table.index and 'Control' in contingency_table.index:
-        odds_ratio = contingency_table.loc['Case', True] * contingency_table.loc['Control', False] / (contingency_table.loc['Control', True] * contingency_table.loc['Case', False])
-        chi2, p, dof, expected = chi2_contingency(contingency_table)
+    contingency_table = pd.crosstab(data['case_or_control'], data[food_type].map({'eat': 'eat', 'not eat': 'not eat'}))
+
+    if 'Control' in contingency_table.index and 'Case' in contingency_table.index:
+        # Calculate chi-square statistic, p-value, and degrees of freedom
+        chi2_stat, p_value, dof, expected = chi2_contingency(contingency_table)
+
+        # Calculate odds ratio
+        odds_ratio = (contingency_table.loc['Control', 'eat'] * contingency_table.loc['Case', 'not eat']) / (contingency_table.loc['Control', 'not eat'] * contingency_table.loc['Case', 'eat'])
+
     else:
-        odds_ratio = chi2 = p = dof = None
-    return odds_ratio, chi2, p
+        odds_ratio = chi2_stat = p_value = None
+
+    return odds_ratio, chi2_stat, p_value, contingency_table
 
 # Function to generate a report displaying chi-square results and odds ratios for each food type
 def generate_chi_square_report(data):
@@ -24,12 +30,13 @@ def generate_chi_square_report(data):
     results = []
 
     for food in food_types:
-        odds_ratio, chi2, p = calculate_odds_ratio_and_chi2(data, food)
+        odds_ratio, chi2_stat, p_value, contingency_table = calculate_odds_ratio_and_chi2(data, food)
         results.append({
             'Food Type': food,
             'Odds Ratio': odds_ratio,
-            'Chi-square': chi2,
-            'P-value': p
+            'Chi-square': chi2_stat,
+            'P-value': p_value,
+            'Contingency Table': contingency_table
         })
 
     report_df = pd.DataFrame(results)
@@ -49,10 +56,10 @@ def main():
         st.subheader("Raw Data")
         st.write(data)
 
-        # Generate and display the Pandas Profiling report
-        if st.sidebar.checkbox("Show Pandas Profiling Report"):
-            st.subheader("Pandas Profiling Report")
-            profile_html = generate_profile_report(data)
+        # Generate and display the Descriptive Statistics report
+        if st.sidebar.checkbox("Show Descriptive Statistics"):
+            st.subheader("Descriptive Statistics of the Data")
+            profile_html = generate_descriptive_statistics(data)
             st.components.v1.html(profile_html, height=600, scrolling=True)
 
         # Show menu to select a specific food type for analysis
@@ -61,16 +68,19 @@ def main():
         selected_food = st.selectbox("Select a food type", food_types)
 
         # Calculate odds ratio and perform chi-square test for the selected food type
-        odds_ratio, chi2, p = calculate_odds_ratio_and_chi2(data, selected_food)
+        odds_ratio, chi2_stat, p_value, contingency_table = calculate_odds_ratio_and_chi2(data, selected_food)
 
-        # Display odds ratio and chi-square test results
-        if odds_ratio is not None and chi2 is not None:
-            st.subheader("Odds Ratio")
-            st.write(f"Odds Ratio for Food Type '{selected_food}': {odds_ratio}")
+        # Generate and display the contingency table for the selected food type
+        if st.sidebar.checkbox("Show Contingency Table"):
+            st.subheader(f"Contingency Table for Food Type '{selected_food}'")
+            contingency_table = contingency_table[['eat', 'not eat']].T
+            contingency_table.index = ['Eat', 'Not Eat']
+            contingency_table.columns = ['Case', 'Control']
+            st.dataframe(contingency_table)
 
-            st.subheader("Chi-square Test Results")
-            st.write(f"Chi-square statistic: {chi2}")
-            st.write(f"P-value: {p}")
+            st.write("Chi-square statistic:", chi2_stat)
+            st.write("P-value:", p_value)
+            st.write("Odds Ratio:", odds_ratio)
 
         # Generate and display the simple report with chi-square results and odds ratios
         if st.sidebar.checkbox("Show Overall Result"):
